6f0d31f32e488908e0640c5a412f05ca0c496900
Vinay K L <61734552+stachyris@users.noreply.github.com>
2021-10-06 16:21:08 +0530

    Add files via upload

---
+++ 5.Trees/Divergence_dating/tree_prep.sh
@@ -0,0 +1,28 @@
+python3 /home/bird_biochemist/soft/mcmctree_tree_prep/mcmctree_tree_prep.py \
+> --left_species athbra \
+> --right_species athblt \
+> --lower_bound 0.036 \
+> --upper_bound 0.053 \
+> --tree /mnt/d/Vinay/Owlet_Phylogeny/Trees/dating/fossil_constraints/Maximum_Likelihood.nwk \
+> python3 mcmctree_tree_prep.py \
+> --left_species bubo_nipalensis_189733 \
+> --right_species bubo_scandiacus_27634 \
+> --lower_bound 0.040 \
+> --upper_bound 0.039 \
+> --tree - \
+> python3 mcmctree_tree_prep.py \
+> --left_species apavit\
+> --right_species athbra \
+> --lower_bound 0.635 \
+> --upper_bound 0.685 \
+> --tree - \
+> --add_header \
+> >owlet_tree_for_mcmct.nwk
+
+
+
+
+
+
+
+
